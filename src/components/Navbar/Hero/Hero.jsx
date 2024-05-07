365e1a9788dd6ad889c3a972354841a35b706f8b
Duy Khánh - 199677 <ddkhanh280200@gmail.com>
2024-05-07 16:33:58 +0700

    commit

--- src/components/Navbar/Hero/Hero.jsx
+++ src/components/Navbar/Hero/Hero.jsx
@@ -1,19 +1,28 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
+import styles from "./Hero.module.css";
 import { getImageUrl } from "../../../utils";
 
 export const Hero = () => {
   return (
-    <section>
-      <div>
-        <h1>Hi, I'm Khanh</h1>
-        <p>
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Khanh</h1>
+        <p className={styles.description}>
           I'm a backend developer with 2 years of experience .NET, Javascript.
           .NET out if you'd like to learn more!
         </p>
-        <a href="mailto:ddkhanh280200@gmail.com">Contact Me</a>
+        <a href="mailto:ddkhanh280200@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
       </div>
-      <img src={getImageUrl("hero/heroImage.png")} alt="Khanh avatar" />
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Khanh avatar"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur}></div>
+      <div className={styles.bottomBlur}></div>
     </section>
   );
 };
